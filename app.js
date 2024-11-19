156aa709f6d8ea4ef5aaae5736660ff5ca170f05
AlexMan123456 <alex.man03888@gmail.com>
2024-11-19 11:15:16 +0000

    Added CORS

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const users = require("./routers/users-router.js")
+const cors = require("cors")
 
+app.use(cors())
 app.use("/api/users", users)
 
 
