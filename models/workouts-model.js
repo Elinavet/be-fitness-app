c64e5074c3146d683a63c799dd8ee17a64b9f756
ivanlougan <robinio123@gmail.com>
2024-11-20 09:17:48 +0000

    added two error handling test cases

--- models/workouts-model.js
+++ models/workouts-model.js
@@ -6,6 +6,9 @@ function fetchWorkouts(userId) {
     return client.connect().then(() => {
         return workoutsDb.find({user_id: new ObjectId(userId)}).toArray();
     }).then((workouts) => {
+        if (workouts.length === 0) {
+            return Promise.reject({status: 404, message: "Workout not found"})
+        }
         return workouts;
     })
 }
