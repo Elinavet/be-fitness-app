42b2840f861c775989dd16cedba7c61503bd7d2f
AlexMan123456 <alex.man03888@gmail.com>
2024-11-22 17:09:28 +0000

    Refactored workouts queries

--- controllers/workouts-controller.js
+++ controllers/workouts-controller.js
@@ -2,21 +2,7 @@ const { fetchWorkoutByLevel, fetchAllWorkouts } = require("../models/workouts-mo
 
 
 function getAllWorkouts(request, response, next) {
-    const { sort_by, order } = request.query;
-
-    const validCols = ["total_duration", "level"]
-    const validOrder = ["ASC", "DESC"]
-
-    const normalizedOrder = order ? order.toUpperCase() : undefined;
-
-    if (sort_by && !validCols.includes(sort_by)) {
-        return response.status(400).send({ message: "Invalid sort by"})
-    }
-    if (normalizedOrder && !validOrder.includes(normalizedOrder)) {
-        return response.status(400).send({ message: "Invalid order"})
-    }
-
-    fetchAllWorkouts(sort_by, normalizedOrder).then((workouts) => {
+    fetchAllWorkouts(request.query).then((workouts) => {
         response.status(200).send({workouts});
     }).catch((err) => {
         next(err)
--- models/workouts-model.js
+++ models/workouts-model.js
@@ -2,11 +2,25 @@ const { ObjectId } = require("mongodb");
 const { client, db } = require("../database/connection.js");
 const workoutsDb = db.collection("workouts");
 
-function fetchAllWorkouts(sort_by = "level", order = "ASC"){
+function fetchAllWorkouts(queries){
+    const validCols = ["total_duration", "level"]
+    const validOrder = ["ASC", "DESC"]
+
+    const sortBy = queries.sort_by ? queries.sort_by : "level"
+    const order = queries.order ? queries.order.toUpperCase() : "ASC"
+
+    if (!validCols.includes(sortBy)) {
+        return Promise.reject({status: 400, message: "Invalid sort by"})
+    }
+    if (!validOrder.includes(order)) {
+        return Promise.reject({status: 400, message: "Invalid order"})
+    }
+
     return client.connect().then(() => {
         return workoutsDb.aggregate([
-            { $sort: { [sort_by]: order === "DESC" ? -1 : 1 }}
+            { $sort: { [sortBy]: order === "DESC" ? -1 : 1 }}
         ]).toArray()
+        //return workoutsDb.find({}).sort({[queries.sort_by]: normalizedOrder === "DESC" ? -1 : 1}).toArray()
     }).then((workouts) => {
         return workouts;
     })
